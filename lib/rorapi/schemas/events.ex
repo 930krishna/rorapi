0eacbe9b6e7dcb579120cb44318a57b9cd8dbe09
930krishna <930krishna@gmail.com>
2020-10-25 15:53:47 +0530

    Create Event schemas.

---
+++ lib/rorapi/schemas/events.ex
@@ -0,0 +1,41 @@
+defmodule Rorapi.Schemas.Events do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @moduledoc "Events Table Model"
+
+  schema "events" do
+    field :description, :string
+    field :type, :string
+    field :event_date, :date
+    field :duration, :string
+    field :event_host, :string
+    field :location, :string
+    field :rsvp_counts, :integer
+    field :rsvp_cancelled_counts, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(events, attrs) do
+    address
+    |> cast( attrs, [:description, :type, :event_date, :duration, :event_host, :location])
+    |> validate_required([:description, :type, :event_date, :duration, :event_host, :location])
+  end
+
+  @doc false
+  def changesetAddRSVP(events, attrs) do
+    address
+    |> cast( attrs, [:rsvp_counts])
+    |> validate_required([:rsvp_counts])
+  end
+
+  @doc false
+  def changesetCancelRSVP(events, attrs) do
+    address
+    |> cast( attrs, [:rsvp_cancelled_counts])
+    |> validate_required([:rsvp_cancelled_counts])
+  end
+
+end
