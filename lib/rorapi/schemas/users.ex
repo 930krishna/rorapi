7b4e33b895bd6786bfbe6f0d8d8f7bbc99277a99
930krishna <930krishna@gmail.com>
2020-10-25 15:54:16 +0530

    Create User schemas.

---
+++ lib/rorapi/schemas/users.ex
@@ -0,0 +1,23 @@
+defmodule Rorapi.Schemas.Users do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @moduledoc "Users Table Model"
+
+  schema "users" do
+    field :full_name, :string
+    field :age, :integer
+    field :email, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(users, attrs) do
+    address
+    |> cast( attrs, [:full_name, :age, :email, :password])
+    |> validate_required([:full_name, :age, :email, :password])
+  end
+
+end
