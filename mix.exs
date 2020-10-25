ea3ce5223966b7588c72491eefb8031dd27ddb23
930krishna <930krishna@gmail.com>
2020-10-25 15:04:42 +0530

    Add plug_cowboy dependencies and update version for cowboy.

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Rorapi.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.1.2"},
+      {:plug_cowboy, "~> 1.0.0"},
     ]
   end
 
@@ -54,7 +55,7 @@ defmodule Rorapi.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
