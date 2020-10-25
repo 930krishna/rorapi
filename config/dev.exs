da73e9415b2d2e736e6a889f6c30b42f5dccea14
930krishna <930krishna@gmail.com>
2020-10-25 15:05:08 +0530

    set db credentials

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,7 @@ config :rorapi, RorapiWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: []
 
 # ## SSL Support
 #
@@ -52,7 +51,7 @@ config :phoenix, :stacktrace_depth, 20
 config :rorapi, Rorapi.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
-  database: "rorapi_dev",
+  password: "sam1186$ABCP",
+  database: "rorapidb",
   hostname: "localhost",
   pool_size: 10
