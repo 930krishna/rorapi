c635ce693190897dc1e0d74301f923327621febb
930krishna <930krishna@gmail.com>
2020-10-27 00:56:16 +0530

    create update method in controller

--- lib/rorapi_web/controllers/admin/events_controller.ex
+++ lib/rorapi_web/controllers/admin/events_controller.ex
@@ -13,9 +13,18 @@ defmodule RorapiWeb.Admin.EventsController do
     with {:ok, message} <- EventRepo.add_event(params) do
       success_response_message(conn, message)
     end
-  end3
+  end
 
-  #Private fuction
+  @doc"""
+     This is for update exist event in db.
+  """
+  def update(conn, params) do
+    with {:ok, message} <- EventRepo.update_event(params) do
+      success_response_message(conn, message)
+    end
+  end
+
+  #Private function
   defp success_response_message(conn, message) do
     conn
     |> put_view(RorapiWeb.SuccessView)
