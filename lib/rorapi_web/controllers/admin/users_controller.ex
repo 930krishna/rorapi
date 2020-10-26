c8221b949d08514e7cc16c147358d3ddfa1cc26c
930krishna <930krishna@gmail.com>
2020-10-27 03:30:48 +0530

    add method for invite user

--- lib/rorapi_web/controllers/admin/users_controller.ex
+++ lib/rorapi_web/controllers/admin/users_controller.ex
@@ -3,11 +3,12 @@ defmodule RorapiWeb.Admin.UsersController do
 
   alias RorapiWeb.Admin.UserView
   alias Rorapi.Models.UserRepo
+  alias Rorapi.Models.EventusersRepo
 
   action_fallback RorapiWeb.FallbackController
 
   @doc"""
-     This is for update exist event in db.
+     This is for user list
   """
   def list(conn, params) do
     with {:ok, value} <- UserRepo.user_list(params) do
@@ -17,5 +18,22 @@ defmodule RorapiWeb.Admin.UsersController do
       |> render("user.json", key: value)
     end
   end
+
+  @doc"""
+     This is for invite user in event
+  """
+  def invite(conn, params) do
+    with {:ok, message} <- EventusersRepo.user_invite(params) do
+      success_response_message(conn, message)
+    end
+  end
+
+  #Private function
+  defp success_response_message(conn, message) do
+    conn
+    |> put_view(RorapiWeb.SuccessView)
+    |> render("200.json", message: message)
+  end
+
 
 end
