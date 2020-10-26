eae0c9302b53064b7fe1b71f0b9973467f467b1d
930krishna <930krishna@gmail.com>
2020-10-27 00:55:52 +0530

    create update event method in repo

--- lib/rorapi/models/event_repo.ex
+++ lib/rorapi/models/event_repo.ex
@@ -5,7 +5,7 @@ defmodule Rorapi.Models.EventRepo do
   alias Rorapi.Schemas.Events
 
   @doc """
-   add Event
+   This is for Add Event.
   """
   def add_event(params) do
     event_map = %{
@@ -16,12 +16,37 @@ defmodule Rorapi.Models.EventRepo do
       event_host: params["host"],
       location: params["location"]
     }
-    changeset_beneficiary = Events.changeset(%Events{}, event_map)
-    case Repo.insert(changeset_beneficiary) do
+    changeset = Events.changeset(%Events{}, event_map)
+    case Repo.insert(changeset) do
       {:ok, _response} ->
         {:ok, "Event Added Successfully"}
       {:error, changeset} ->
         {:error, changeset}
     end
   end
+
+  @doc """
+    This is for Update Event.
+  """
+  def update_event(params) do
+    case Repo.get_by(Events, id: params["id"]) do
+      nil -> {:error_message, :message, "Event Not Found"}
+      events -> event_map = %{
+         description: params["description"],
+         type: params["type"],
+         event_date: params["date"],
+         duration: params["duration"],
+         event_host: params["host"],
+         location: params["location"]
+      }
+      changeset = Events.changeset(events, event_map)
+      case Repo.update(changeset) do
+          {:ok, _response} ->
+            {:ok, "Event Updated Successfully"}
+          {:error, changeset} ->
+            {:error, changeset}
+      end
+    end
+  end
+
 end
