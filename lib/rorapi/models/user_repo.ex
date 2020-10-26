4560d848131b0c0bbe005f688a5539ce85694811
930krishna <930krishna@gmail.com>
2020-10-27 02:14:58 +0530

    Create add_user method for insert user in db.

---
+++ lib/rorapi/models/user_repo.ex
@@ -0,0 +1,37 @@
+defmodule Rorapi.Models.UserRepo do
+  import Ecto.Query
+
+  alias Rorapi.Repo
+  alias Rorapi.Schemas.Users
+
+  @doc """
+   This is for Add User.
+  """
+  def add_user(params) do
+    user_map = %{
+      full_name: params["full_name"],
+      age: params["age"],
+      email: params["email"],
+      password: params["password"]
+    }
+    changeset = Users.changeset(%Users{}, user_map)
+    case Repo.insert(changeset) do
+      {:ok, _response} ->
+        {:ok, "Registration Successfully"}
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  @doc"""
+     This is for list users.
+  """
+  def user_list(params)do
+    get =  Users
+           |> select([a], a)
+           |> order_by([a], desc: a.id)
+           |> Repo.paginate(params)
+    {:ok, get}
+  end
+
+end
