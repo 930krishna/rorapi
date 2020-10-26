fb4a72c581794a7c33cc13e7f025de40aee07f61
930krishna <930krishna@gmail.com>
2020-10-27 01:28:13 +0530

    set page_size and add adapter

--- lib/rorapi/repo.ex
+++ lib/rorapi/repo.ex
@@ -1,11 +1,13 @@
 defmodule Rorapi.Repo do
-  use Ecto.Repo, otp_app: :rorapi
+  use Scrivener, page_size: 30
+  use Ecto.Repo, otp_app: :rorapi,
+  adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-  end
+#  def init(_, opts) do
+#    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+#  end
 end
