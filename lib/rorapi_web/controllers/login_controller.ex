029ded09fcf9ce222872f92f3326b992b0fa6515
930krishna <930krishna@gmail.com>
2020-10-27 04:04:22 +0530

    admin login method call

---
+++ lib/rorapi_web/controllers/login_controller.ex
@@ -0,0 +1,26 @@
+defmodule RorapiWeb.LoginController do
+  use RorapiWeb, :controller
+
+  alias Rorapi.Models.AdminRepo
+  alias Rorapi.Models.UserRepo
+
+  action_fallback RorapiWeb.FallbackController
+
+  @doc"""
+     This is for admin login
+  """
+  def admin_login(conn, params) do
+    with {:ok, output} <- AdminRepo.view(params) do
+      json conn, %{status: 200, fullname: output.full_name, message: "Login Successfully"}
+    end
+  end
+
+  @doc"""
+     This is for user login
+  """
+  def user_login(conn, params) do
+    text conn, "user login"
+  end
+
+
+end
