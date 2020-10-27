79d6d107c912bf8f6aa9d0fe8796f0d87d799be0
930krishna <930krishna@gmail.com>
2020-10-27 18:04:27 +0530

    New method for attend and cancelled user list in repo with where clause.

--- lib/rorapi/models/user_repo.ex
+++ lib/rorapi/models/user_repo.ex
@@ -3,6 +3,7 @@ defmodule Rorapi.Models.UserRepo do
 
   alias Rorapi.Repo
   alias Rorapi.Schemas.Users
+  alias Rorapi.Schemas.Eventusers
 
   @doc """
    This is for Add User.
@@ -41,6 +42,40 @@ defmodule Rorapi.Models.UserRepo do
     case Repo.get_by(Users, email: params["email"], password: params["password"]) do
       nil -> {:error_message, :message, "Email or Password is wrong."}
       admin ->  {:ok, admin}
+    end
+  end
+
+  @doc"""
+     This is for list of attend users.
+  """
+  def attend_list(params)do
+    attend = Repo.get_by(Eventusers, events_id: params["event_id"])
+    case attend do
+      nil -> {:error_message, :message, "Event id is not exist."}
+      admin ->  attend_user = attend.confirmed_users
+                get =  Users
+                       |> where([p], p.id in ^attend_user)
+                       |> select([a], a)
+                       |> order_by([a], desc: a.id)
+                       |> Repo.paginate(params)
+                {:ok, get}
+    end
+  end
+
+  @doc"""
+     This is for list of cancelled users.
+  """
+  def cancelled_list(params)do
+    cancelled = Repo.get_by(Eventusers, events_id: params["event_id"])
+    case cancelled do
+      nil -> {:error_message, :message, "Event id is not exist."}
+      admin ->  cancelled_user = cancelled.cancelled_users
+                get =  Users
+                       |> where([p], p.id in ^cancelled_user)
+                       |> select([a], a)
+                       |> order_by([a], desc: a.id)
+                       |> Repo.paginate(params)
+                {:ok, get}
     end
   end
 
